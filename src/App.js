4b4fa118d0f80fccb149b6b5d448f810366a2360
Soo-Yeon Hong <tndus_03@naver.com>
2022-01-07 23:05:32 +0900

    todo 앱

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ import TodoList from './components/TodoList';
 
 const App = () => {
   const [todos, setTodos] = useState([
-    // {
-    //   id: 1,
-    //   text: '리액트의 기초 알아보기',
-    //   checked: true,
-    // },
-    // {
-    //   id: 2,
-    //   text: '컴포넌트 스타일링 해보기',
-    //   checked: true,
-    // },
-    // {
-    //   id: 3,
-    //   text: '일정 관리 앱 만들어 보기',
-    //   checked: false,
-    // },
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
   ]);
 
   const nextId = useRef(1);
